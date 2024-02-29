390a2be82bd48dd64597faad95c6d1d531996a89
marciliodevjava <marciliodevjava@gmail.com>
2024-02-29 20:55:31 -0300

    Update config

--- config/config.py
+++ config/config.py
@@ -1,5 +1,7 @@
 from flask_restful import Api
 
+from config.banco_de_dados import db
+from config.jwt import jwt
 from variaveis.variaves import DATABASE_URI, SQLALCHEMY_DATABASE_URI, HEADER_NAME, HEADER_TYPE, NM_ID_SESSAO, NONE
 
 
@@ -7,4 +9,8 @@ def config_app(api: Api):
     api.app.config[DATABASE_URI] = SQLALCHEMY_DATABASE_URI
     api.app.config[HEADER_NAME] = NM_ID_SESSAO
     api.app.config[HEADER_TYPE] = NONE
+
+    db.init_app(api.app)
+    jwt.init_app(api.app)
+
     return api
