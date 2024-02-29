0381210ea43b5b8fac8beee84d6eb2e01fa1eef9
marciliodevjava <marciliodevjava@gmail.com>
2024-02-28 22:37:19 -0300

    Update app

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 
+from config.config import config_app
 from route.app_v0 import config_routes_v0
 
 app = Flask(__name__)
 api = Api(app)
+
+api = config_app(api)
 
 api = config_routes_v0(api)
 
