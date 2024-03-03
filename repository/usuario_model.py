85c1ad150bef062a528d266ac06bc85cb8db75a8
marciliodevjava <marciliodevjava@gmail.com>
2024-03-03 20:47:15 -0300

    Update usuario_model

--- repository/usuario_model.py
+++ repository/usuario_model.py
@@ -2,7 +2,7 @@ from config.banco_de_dados import db
 
 
 class UsuarioModel(db.Model):
-    __tablename__: 'usuario'
+    __tablename__ = 'usuario'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(150), nullable=False)
     login = db.Column(db.String(100), nullable=False)
