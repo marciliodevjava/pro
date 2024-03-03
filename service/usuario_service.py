79dd0944ef06364cfd0fde0a37833ce709db7b9b
marciliodevjava <marciliodevjava@gmail.com>
2024-03-03 20:15:36 -0300

    Update usuario_service

--- service/usuario_service.py
+++ service/usuario_service.py
@@ -1,3 +1,26 @@
+from enumerate.message import UsuarioFormulario
+from repository.usuario_model import UsuarioModel
+from utils.encriptador_de_senha import gerador_password_hash
+
 
 class UsuarioService:
-    pass
+    @classmethod
+    def cadastro_ususario(cls, dados):
+        login = UsuarioModel.buscar(dados['login'])
+        if login:
+            return {
+                'message': UsuarioFormulario.USUARIO_JA_EXISTE
+            }
+        nome = dados['nome']
+        login = dados['login']
+        senha = gerador_password_hash(dados['senha'])
+        email = dados['email']
+
+        usuario = UsuarioModel(nome=nome, login=login, senha=senha, email=email)
+        try:
+            usuario = UsuarioModel.salvar(usuario)
+            if usuario:
+                return {
+                    'message': UsuarioFormulario.USUARIO_CRIADO,
+                    'usuario':
+                }
