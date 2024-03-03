460ca7f5d696649a9bc54ad621e923f292d5c44b
marciliodevjava <marciliodevjava@gmail.com>
2024-03-03 19:46:48 -0300

    Update login_resource

--- resource/login_resource.py
+++ resource/login_resource.py
@@ -1,7 +1,15 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+
+from enumerate.message import UsuarioFormulario
 
 
 class Login(Resource):
+    def __init__(self):
+        self.__parser = reqparse.RequestParser()
+        self.__parser.add_argument('nome', type=str, required=True, help=UsuarioFormulario.O_CAMPO_USERNAME)
+        self.__parser.add_argument('senha', type=str, required=True, help=UsuarioFormulario.O_CAMPO_PASSWORD)
+        self.__parser.add_argument('email', type=str, required=True, help=UsuarioFormulario.O_CAMPO_EMAIL)
+
     def get(self):
         pass
 
