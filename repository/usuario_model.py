b531e7483e2df11e310d8a59e714fd285bd4c835
marciliodevjava <marciliodevjava@gmail.com>
2024-03-03 20:15:28 -0300

    Update usuario_model

--- repository/usuario_model.py
+++ repository/usuario_model.py
@@ -5,10 +5,38 @@ class UsuarioModel(db.Model):
     __tablename__: 'usuario'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(150), nullable=False)
+    login = db.Column(db.String(100), nullable=False)
     senha = db.Column(db.String(35), nullable=False)
     email = db.Column(db.String(255), nullable=False)
 
-    def __init__(self, nome, senha, email):
+    def __init__(self, nome, login, senha, email):
         self.nome = nome
+        self.login = login
         self.senha = senha
         self.email = email
+
+    def json(self):
+        return {
+            'nome': self.nome,
+            'login': self.login,
+            'email': self.email
+        }
+
+    @classmethod
+    def buscar(cls, login):
+        try:
+            login = db.session.query(cls).filter_by(login=login).first()
+            if login:
+                return login
+            return None
+        except BaseException as b:
+            return None
+
+    @classmethod
+    def salvar(cls, usuario):
+        try:
+            db.session.add(usuario)
+            db.session.commit()
+            return usuario
+        except BaseException as b:
+            return None
