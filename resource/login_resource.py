f5cc3b9ca0245e88edf02895bc9b78bb433d03cb
marciliodevjava <marciliodevjava@gmail.com>
2024-03-03 20:15:10 -0300

    Update login_resource

--- resource/login_resource.py
+++ resource/login_resource.py
@@ -1,12 +1,14 @@
 from flask_restful import Resource, reqparse
 
 from enumerate.message import UsuarioFormulario
+from service.usuario_service import UsuarioService
 
 
 class Login(Resource):
     def __init__(self):
         self.__parser = reqparse.RequestParser()
         self.__parser.add_argument('nome', type=str, required=True, help=UsuarioFormulario.O_CAMPO_USERNAME)
+        self.__parser.add_argument('login', type=str, required=True, help=UsuarioFormulario.O_CAMPO_LOGIN)
         self.__parser.add_argument('senha', type=str, required=True, help=UsuarioFormulario.O_CAMPO_PASSWORD)
         self.__parser.add_argument('email', type=str, required=True, help=UsuarioFormulario.O_CAMPO_EMAIL)
 
@@ -17,7 +19,11 @@ class Login(Resource):
         pass
 
     def cadastro(self):
-        pass
+        dados = self.__parser.parse_args()
+        usuario = UsuarioService.cadastro_ususario(dados)
+        if usuario.get('message') == UsuarioFormulario.USUARIO_JA_EXISTE:
+            return usuario, 200
+        return usuario
 
     def put(self):
         pass
