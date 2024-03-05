70403363a94129164d538d272be50397416041ec
marciliodevjava <marciliodevjava@gmail.com>
2024-03-05 12:25:13 -0300

    Update app_v0

--- route/app_v0.py
+++ route/app_v0.py
@@ -1,12 +1,12 @@
 from flask_restful import Api
 
-from resource.login_resource import Login
+from resource.login_resource import CadastroResource, LoginResource
 from variaveis.variaves import BASE_PATH_HTTP
 
 
 def config_routes_v0(api: Api):
-    api.add_resource(Login, f'{BASE_PATH_HTTP}/login', methods=["GET"], endpoint='get')
-    api.add_resource(Login, f'{BASE_PATH_HTTP}/cadastro', methods=["POST"], endpoint='post')
-    api.add_resource(Login, f'{BASE_PATH_HTTP}/login', methods=["PUT"], endpoint='put')
-    api.add_resource(Login, f'{BASE_PATH_HTTP}/login', methods=["DELETE"], endpoint='delete')
+    api.add_resource(CadastroResource, f'{BASE_PATH_HTTP}/cadastro', methods=["POST"], endpoint='cadastro_usuario')
+    api.add_resource(CadastroResource, f'{BASE_PATH_HTTP}/cadastro', methods=["PUT"], endpoint='alterar_usuario')
+    api.add_resource(CadastroResource, f'{BASE_PATH_HTTP}/cadastro', methods=["DELETE"], endpoint='deletar_usuario')
+    api.add_resource(LoginResource, f'{BASE_PATH_HTTP}/login', methods=["POST"], endpoint='login')
     return api
