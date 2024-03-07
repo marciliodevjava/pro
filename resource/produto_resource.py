0181cdbfb56b49f477add4b8fe3ee506c133a194
marciliodevjava <marciliodevjava@gmail.com>
2024-03-07 18:34:49 -0300

    Create produto_resource

--- resource/produto_resource.py
+++ resource/produto_resource.py
@@ -1,10 +1,11 @@
 from flask_restful import Resource, request
 from fomulario.produto_shema import ProdutoShema
-
+from service.produto_service import ProdutoService
 
 class ProdutoResource(Resource):
     def post(self):
         dados = ProdutoShema.load(request.json)
+        produto = ProdutoService.cadastro_produto(dados)
 
     def get(self):
         pass
