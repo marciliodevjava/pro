53783ffa7c88fe5c3e74e2078b5e2300f107ca4f
marciliodevjava <marciliodevjava@gmail.com>
2024-03-07 17:59:13 -0300

    Update app_v0

--- route/app_v0.py
+++ route/app_v0.py
@@ -1,6 +1,7 @@
 from flask_restful import Api
 
 from resource.login_resource import CadastroResource, LoginResource
+from resource.produto_resource import ProdutoResource
 from variaveis.variaves import BASE_PATH_HTTP
 
 
@@ -9,4 +10,9 @@ def config_routes_v0(api: Api):
     api.add_resource(CadastroResource, f'{BASE_PATH_HTTP}/cadastro', methods=["PUT"], endpoint='alterar_usuario')
     api.add_resource(CadastroResource, f'{BASE_PATH_HTTP}/cadastro', methods=["DELETE"], endpoint='deletar_usuario')
     api.add_resource(LoginResource, f'{BASE_PATH_HTTP}/login', methods=["POST"], endpoint='login')
+
+    api.add_resource(ProdutoResource, f'{BASE_PATH_HTTP}/produto', methods=['POST'], endpoint='cadastro_produto')
+    api.add_resource(ProdutoResource, f'{BASE_PATH_HTTP}/produto', methods=['PUT'], endpoint='alterar_produto')
+    api.add_resource(ProdutoResource, f'{BASE_PATH_HTTP}/produto', methods=['GET'], endpoint='buscar_produto')
+    api.add_resource(ProdutoResource, f'{BASE_PATH_HTTP}/produto', methods=['DELETE'], endpoint='deletar_produto')
     return api
