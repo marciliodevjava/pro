0b43505d32e60e09d66fb0196fd48b59c96dd354
marciliodevjava <marciliodevjava@gmail.com>
2024-03-08 20:20:14 -0300

    Update config

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from config.banco_de_dados import db
 from config.jwt import jwt
 from variaveis.variaves import DATABASE_URI, SQLALCHEMY_DATABASE_URI, HEADER_NAME, HEADER_TYPE, NM_ID_SESSAO, NONE, \
-    SECRET, SECRET_KEY
+    SECRET, SECRET_KEY, TOKEN, TIME_TOKEN
 
 
 def config_app(api: Api):
@@ -11,6 +11,7 @@ def config_app(api: Api):
     api.app.config[HEADER_NAME] = NM_ID_SESSAO
     api.app.config[HEADER_TYPE] = NONE
     api.app.config[SECRET] = SECRET_KEY
+    api.app.config[TOKEN] = TIME_TOKEN
 
     db.init_app(api.app)
     jwt.init_app(api.app)
