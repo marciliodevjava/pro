68cafc809e0bd337c11006f237fe0a7671a01be4
marciliodevjava <marciliodevjava@gmail.com>
2024-03-08 20:58:26 -0300

    Update app

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_restful import Api
 from jwt import ExpiredSignatureError
 from werkzeug.exceptions import HTTPException
 
 from config.config import config_app
 from route.app_v0 import config_routes_v0
+from utils.response_erro import ResponseErro
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,14 +17,14 @@ api = config_routes_v0(api)
 
 @app.errorhandler(ExpiredSignatureError)
 def handle_expired_signature_error(error):
-    response = jsonify({'error': 'Token expirado', 'message': 'Faça login novamente para obter um novo token.'})
-    response.status_code = 401
+    response = ResponseErro.token_expirado()
+    response.status_code = getattr(error, 'code', 401)
     return response
 
 
 @app.errorhandler(HTTPException)
 def handle_http_exception(error):
-    response = jsonify({'error': 'Erro interno no servidor', 'message': 'Ocorreu um erro interno no servidor.'})
+    response = ResponseErro.http_exception()
     response.status_code = getattr(error, 'code', 500)
     return response
 
