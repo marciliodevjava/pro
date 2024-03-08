46680c05bd6f48dddd389c748087089d41a13ae9
marciliodevjava <marciliodevjava@gmail.com>
2024-03-08 20:21:52 -0300

    Update produto_resource

--- resource/produto_resource.py
+++ resource/produto_resource.py
@@ -1,6 +1,5 @@
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource, request
-from jwt import ExpiredSignatureError
 
 from enumerate.message import ProdutoMessage
 from fomulario.produto_shema import ProdutoShema
@@ -11,24 +10,15 @@ from mapper.produto_mapper import ProdutoMapper
 class ProdutoResource(Resource):
     @jwt_required()
     def post(self):
-        try:
-            dados = ProdutoShema().load(request.json)
-            produto = ProdutoService.cadastro_produto(dados)
-            if produto.get('message').__eq__(ProdutoMessage.PRODUTO_EXISTENTE.value):
-                return produto, 200
-            if not produto:
-                return {
-                    'message': ProdutoMessage.PRODUTO_OCORREU_UM_ERRO
-                }
-            return produto, 201
-        except ExpiredSignatureError:
+        dados = ProdutoShema().load(request.json)
+        produto = ProdutoService.cadastro_produto(dados)
+        if produto.get('message').__eq__(ProdutoMessage.PRODUTO_EXISTENTE.value):
+            return produto, 200
+        if not produto:
             return {
-                'message': 'Token expirado. Faça login novamente para obter um novo token.'
-            }, 401
-        except Exception as e:
-            return {
-                'message': f'Ocorreu um erro: {str(e)}'
-            }, 500
+                'message': ProdutoMessage.PRODUTO_OCORREU_UM_ERRO
+            }
+        return produto, 201
 
     def get(self):
         produto = ProdutoService.buscar_todos()
