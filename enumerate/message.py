7f0b9069c6d3854f7c820e9c6460c1c80db49458
marciliodevjava <marciliodevjava@gmail.com>
2024-03-09 21:33:41 -0300

    Update message

--- enumerate/message.py
+++ enumerate/message.py
@@ -1,59 +1,61 @@
 from enum import Enum
 
+from variables.variaveis_enum import LOGIN, SENHA, EMAIL, NONE, PRECO, DESCRICAO, QUANTIDADE, USUARIO, PRODUTO, TOKEN
+
 
 class MessageLogin(Enum):
-    USUARIO_NAO_EXISTE = 'Usuário não existe, deseja criar esse usuário?'
+    USUARIO_NAO_EXISTE = f'{USUARIO} não existe, deseja criar esse {USUARIO}?'
     LOGIN_SENHA_INCORRETA = 'Senha incorreta, tente novamente.'
-    LOGIN_EFETUADO_COM_SUCESSO = 'O Login foi realizado com sucesso.'
+    LOGIN_EFETUADO_COM_SUCESSO = 'Login foi realizado com sucesso.'
     LOGIN_OCORREU_ERRO_AO_LOGAR = 'Ocorreu um erro ao logar, informe ao suporte.'
-    LOGIN_USUARIO_DELETADO_COM_SUCESSO = 'Usuário deletado com sucesso.'
-    LOGIN_USUARIO_ATUALIZADO_COM_SUCESSO = 'Usuário atualizado com sucesso.'
-    LOGIN_USUARIO_CRIADO_COM_SUCESSO = 'Usuário criado com sucesso.'
+    LOGIN_USUARIO_DELETADO_COM_SUCESSO = f'{USUARIO} deletado com sucesso.'
+    LOGIN_USUARIO_ATUALIZADO_COM_SUCESSO = f'{USUARIO} atualizado com sucesso.'
+    LOGIN_USUARIO_CRIADO_COM_SUCESSO = f'{USUARIO} criado com sucesso.'
 
 
 class MessageToken(Enum):
-    TOKEN_INVALIDO = 'O Token é inválido'
-    TOKEN_EXPIRADO = 'O Token está expirado.'
-    TOKEN_NAO_ENVIADO = 'O Token não foi enviado.'
-    TOKEN_LOGIN_NOVAMENTE = 'Faça login novamente para obter um novo token'
+    TOKEN_INVALIDO = f'O {TOKEN} é inválido'
+    TOKEN_EXPIRADO = f'O {TOKEN} está expirado.'
+    TOKEN_NAO_ENVIADO = f'O {TOKEN} não foi enviado.'
+    TOKEN_LOGIN_NOVAMENTE = f'Faça {LOGIN} novamente para obter um novo {TOKEN}'
 
 
 class UsuarioFormulario(Enum):
-    USUARIO_OCORREU_UM_ERRO_PARA_DELETAR = 'Ocorreu um erro ao deletar o usuário.'
-    USUARIO_DELETADO_COM_SUCESSO = 'Usuário deletado com sucesso.'
-    USUARIO_NAO_ATUALIZADO = 'Usuário não encontrado para atualizar.'
-    USUARIO_ATUALIZADO = 'Usuário atualizado com sucesso.'
-    USUARIO_OCORREU_UM_ERRO = 'Ocorreu um erro ao salvar usuario.'
-    USUARIO_JA_EXISTE = 'Usuario já existe.'
-    USUARIO_CRIADO = 'Usuario criado com sucesso.'
-    O_CAMPO_LOGIN = 'O campo login não foi enviado.'
-    O_CAMPO_EMAIL = 'O campo email não foi enviado.'
-    O_CAMPO_PASSWORD = 'O campo senha não foi enviado.'
-    O_CAMPO_USERNAME = 'O campo nome não foi enviado.'
+    USUARIO_OCORREU_UM_ERRO_PARA_DELETAR = f'Ocorreu um erro ao deletar o {USUARIO}.'
+    USUARIO_DELETADO_COM_SUCESSO = f'{USUARIO} deletado com sucesso.'
+    USUARIO_NAO_ATUALIZADO = f'{USUARIO} não encontrado para atualizar.'
+    USUARIO_ATUALIZADO = f'{USUARIO} atualizado com sucesso.'
+    USUARIO_OCORREU_UM_ERRO = f'Ocorreu um erro ao salvar {USUARIO}.'
+    USUARIO_JA_EXISTE = f'{USUARIO} já existe.'
+    USUARIO_CRIADO = f'{USUARIO} criado com sucesso.'
+    O_CAMPO_LOGIN = f'O campo {LOGIN} não foi enviado.'
+    O_CAMPO_EMAIL = f'O campo {EMAIL} não foi enviado.'
+    O_CAMPO_PASSWORD = f'O campo {SENHA} não foi enviado.'
+    O_CAMPO_USERNAME = f'O campo {NONE} não foi enviado.'
 
 
 class LoginFormulario(Enum):
-    CAMPO_SENHA = 'O campo login não foi enviado.'
-    CAMPO_LOGIN = 'O campo senha não foi enviado.'
+    CAMPO_SENHA = f'O campo {SENHA} não foi enviado.'
+    CAMPO_LOGIN = f'O campo {LOGIN} não foi enviado.'
 
 
 class ProdutoFormulario(Enum):
-    O_CAMPO_PRECO = 'O campo preço não foi enviado.'
-    O_CAMPO_QUANTIDADE = 'O campo quantidade não foi enviado.'
-    O_CAMPO_DESCRICAO = 'O campo descrição não foi enviado.'
-    O_CAMPO_NOME = 'O campo nome não foi enviado.'
+    O_CAMPO_PRECO = f'O campo {PRECO} não foi enviado.'
+    O_CAMPO_QUANTIDADE = f'O campo {QUANTIDADE} não foi enviado.'
+    O_CAMPO_DESCRICAO = f'O campo {DESCRICAO} não foi enviado.'
+    O_CAMPO_NOME = f'O campo {NONE} não foi enviado.'
 
 
 class ProdutoMessage(Enum):
-    OCORREU_UM_ERRO_AO_DELETAR_PRODUTO = 'Ocorreu um erro ao deletar o Produto.'
-    PRODUTO_DELETADO_COM_SUCESSO = 'Produto deletado com sucesso.'
-    NAO_EXISTE_ESSE_PRODUTO_PARA_ATUALIZAR = 'Não existe esse produto para ser atualizado.'
-    OCORREU_UM_ERRO_AO_ATUALIZAR_PRODUTO = 'Ocorreu um erro ao atualizar esse produto.'
-    PRODUTO_ATUALIZADO_COM_SUCESSO = 'Produto atualizado com sucesso.'
-    NAO_EXISTE_PRODUTOS_CADASTRADOS = 'Não existe produto cadastrados.'
-    PRODUTO_OCORREU_UM_ERRO = 'Ocorreu um erro ao salvar produto.'
-    PRODUTO_CRIADO_COM_SUCESSO = 'Produto criado com sucesso.'
-    PRODUTO_EXISTENTE = 'Esse produto já foi cadastrado.'
+    OCORREU_UM_ERRO_AO_DELETAR_PRODUTO = f'Ocorreu um erro ao deletar o {PRODUTO}.'
+    PRODUTO_DELETADO_COM_SUCESSO = f'{PRODUTO} deletado com sucesso.'
+    NAO_EXISTE_ESSE_PRODUTO_PARA_ATUALIZAR = f'Não existe esse {PRODUTO} para ser atualizado.'
+    OCORREU_UM_ERRO_AO_ATUALIZAR_PRODUTO = f'Ocorreu um erro ao atualizar esse {PRODUTO}.'
+    PRODUTO_ATUALIZADO_COM_SUCESSO = f'{PRODUTO} atualizado com sucesso.'
+    NAO_EXISTE_PRODUTOS_CADASTRADOS = f'Não existe esse {PRODUTO} cadastrados.'
+    PRODUTO_OCORREU_UM_ERRO = f'Ocorreu um erro ao salvar {PRODUTO}.'
+    PRODUTO_CRIADO_COM_SUCESSO = f'{PRODUTO} criado com sucesso.'
+    PRODUTO_EXISTENTE = f'Esse {PRODUTO} já foi cadastrado.'
 
 
 class ErroServidorMessage(Enum):
