e0c8d8e69a2e93a23b1a77415054399dd8964fe4
marciliodevjava <marciliodevjava@gmail.com>
2024-03-11 19:55:53 -0300

    Update afiliado_shema

--- form/afiliado_shema.py
+++ form/afiliado_shema.py
@@ -1,3 +1,10 @@
-from marshmallow import Schema
+from marshmallow import Schema, fields
+
+from enumerate.message import AfiliadoFormulario
+
+
 class AfiliadoShema(Schema):
-    pass
+    nome = fields.Str(required=True, error_messages={'required': AfiliadoFormulario.CAMPO_NOME})
+    email = fields.Str(required=True, error_messages={'required': AfiliadoFormulario.CAMPO_EMAIL})
+    cpf = fields.Str(required=True, error_messages={'required': AfiliadoFormulario.CAMPO_CPF})
+    rg = fields.Str(required=True, error_messages={'required': AfiliadoFormulario.CAMPO_PRECO})
