2e7ddd2e491a7060699089629f2c09ab137437d2
marciliodevjava <marciliodevjava@gmail.com>
2024-03-10 22:58:19 -0300

    Create afiliado_service

---
+++ service/afiliado_service.py
@@ -0,0 +1,16 @@
+class AfiliadoService:
+    @classmethod
+    def cadastrar_afiliado(cls):
+        pass
+
+    @classmethod
+    def buscar_afiliado(cls):
+        pass
+
+    @classmethod
+    def atualizar_afiliado(cls):
+        pass
+
+    @classmethod
+    def deletar_afiliado(cls):
+        pass
