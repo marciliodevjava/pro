664b16776a5bbe5eb7308fcaa2c58693389641f3
marciliodevjava <marciliodevjava@gmail.com>
2024-03-11 19:19:17 -0300

    Update app_v0

--- route/app_v0.py
+++ route/app_v0.py
@@ -2,6 +2,7 @@ from flask_restful import Api
 
 from resource.login_resource import CadastroResource, LoginResource
 from resource.produto_resource import ProdutoResource
+from resource.afiliados_resource import AfiliadoResource
 from variables.variaves_do_sistema import BASE_PATH_HTTP
 
 
@@ -15,4 +16,9 @@ def config_routes_v0(api: Api):
     api.add_resource(ProdutoResource, f'{BASE_PATH_HTTP}/produto/<int:id>', methods=['PUT'], endpoint='alterar_produto')
     api.add_resource(ProdutoResource, f'{BASE_PATH_HTTP}/produto', methods=['GET'], endpoint='buscar_produto')
     api.add_resource(ProdutoResource, f'{BASE_PATH_HTTP}/produto/<int:id>', methods=['DELETE'], endpoint='deletar_produto')
+
+    api.add_resource(AfiliadoResource, f'{BASE_PATH_HTTP}/afiliado', methods=['POST'], endpoint='cadastro_afiliado')
+    api.add_resource(AfiliadoResource, f'{BASE_PATH_HTTP}/afiliado', methods=['PUT'], endpoint='alterar_afiliado')
+    api.add_resource(AfiliadoResource, f'{BASE_PATH_HTTP}/afiliado/<int:id>', methods=['GET'], endpoint='buscar_afiliado')
+    api.add_resource(AfiliadoResource, f'{BASE_PATH_HTTP}/afiliado', methods=['DELETE'], enpoint='deletar_afiliado')
     return api
