dca4fa3078f4de1a0c2af6142f674d3434d7b144
marciliodevjava <marciliodevjava@gmail.com>
2024-03-11 20:02:55 -0300

    Update afiliado_model

---
+++ repository/afiliado_model.py
@@ -0,0 +1,16 @@
+from config.config import db
+
+
+class AfiliadoModel(db.Model):
+    __tablename__ = 'afiliado'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome_completo = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    cpf = db.Column(db.String(11), nullable=False)
+    rg = db.Column9(db.String(15), nullable=False)
+
+    def __init__(self, nome, email, cpf, rg):
+        self.nome_completo = nome
+        self.email = email
+        self.cpf = cpf
+        self.rg = rg
