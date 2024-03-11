0feb5add58e7dae226a87f8d7d5f02b448a84f69
marciliodevjava <marciliodevjava@gmail.com>
2024-03-11 20:44:42 -0300

    Update afiliado_service

--- service/afiliado_service.py
+++ service/afiliado_service.py
@@ -1,7 +1,15 @@
+from repository.afiliado_model import AfiliadoModel
 class AfiliadoService:
     @classmethod
-    def cadastrar_afiliado(cls):
-        pass
+    def cadastrar_afiliado(cls, dados):
+        nome = dados['nome']
+        email = dados['email']
+        cpf = dados['cpf']
+        rg = dados['rg']
+        afiliado = AfiliadoModel(nome, email, cpf, rg)
+        afiliado = AfiliadoModel.salvar(afiliado)
+        if afiliado:
+
 
     @classmethod
     def buscar_afiliado(cls):
