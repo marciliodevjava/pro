865f71b6f03fcb16d6f1f7e1ceb18ccd867f757f
marciliodevjava <marciliodevjava@gmail.com>
2024-03-10 22:58:38 -0300

    Create afiliados_resource

---
+++ resource/afiliados_resource.py
@@ -0,0 +1,16 @@
+from flask_restful import Resource
+
+
+class AfiliadoResource(Resource):
+
+    def post(self):
+        pass
+
+    def get(self):
+        pass
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
